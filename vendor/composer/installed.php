64a3caa050ad5f0874cf59f9ef512570988595b7
Jasiel <kaiserflex@gmail.com>
2025-11-26 20:49:49 -0500

    tercer commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'arcade/retro',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '36d8326bfdeb20cd6a9f73d789f43b997182d711',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'arcade/retro' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '36d8326bfdeb20cd6a9f73d789f43b997182d711',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
